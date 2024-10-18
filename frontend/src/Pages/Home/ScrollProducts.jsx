bfefc8d5708f104f3e7d96a49c2b50d75df4d93a
aravind <aravind284479@gmail.com>
2024-10-18 20:03:03 +0530

    pink shadow

--- frontend/src/Pages/Home/ScrollProducts.jsx
+++ frontend/src/Pages/Home/ScrollProducts.jsx
@@ -31,7 +31,7 @@ const ScrollProducts = ({ products }) => {
           );
         })}
       {products.length > 4 && (
-        <div className="min-w-48 p-5 h-58 shadow-md rounded-2xl cursor-pointer shadow-pink-400 bg-white flex justify-center items-center flex-col text-center">
+        <div className="min-w-48 p-5 h-58 shadow-sm rounded-2xl cursor-pointer shadow-pink-400 bg-white flex justify-center items-center flex-col text-center">
           <ArrowForwardIcon fontSize="large" />
           <p>More</p>
         </div>

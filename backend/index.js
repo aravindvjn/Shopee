53973854c3443d64fa4a78bd0394ba34fac5e1ba
aravind <aravind284479@gmail.com>
2024-10-21 21:48:16 +0530

    test 1

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import pkg from "pg";
 import cors from "cors";
-import authRoutes from "./routes/auth.js";
-import cartRoutes from "./routes/cart.js";
+// import authRoutes from "./routes/auth.js";
+// import cartRoutes from "./routes/cart.js";
 
 dotenv.config();
 const { Pool } = pkg;
@@ -16,20 +16,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Database connection
-// export const pool = new Pool({
-//   connectionString: process.env.DB_URL,
-// });
+export const pool = new Pool({
+  connectionString: process.env.DB_URL,
+});
 
 // Connecting to the database
-// pool
-//   .connect()
-//   .then(() => console.log("Connected to the database successfully!"))
-//   .catch((err) => console.error("Connection error", err.stack));
+pool
+  .connect()
+  .then(() => console.log("Connected to the database successfully!"))
+  .catch((err) => console.error("Connection error", err.stack));
 
-// pool.on("error", (err, client) => {
-//   console.error("Unexpected error on idle client", err);
-//   process.exit(-1);
-// });
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
 // Root route
 app.get("/", (req, res) => {
@@ -37,14 +37,14 @@ app.get("/", (req, res) => {
 });
 
 // Routes
-app.use("/", authRoutes);
-app.use("/cart", cartRoutes);
+// app.use("/", authRoutes);
+// app.use("/cart", cartRoutes);
 
 // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Something went wrong!");
-// });
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 // Listening
 app.listen(PORT, () => {
